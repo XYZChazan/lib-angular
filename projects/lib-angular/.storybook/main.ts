f27d92cb44bb44da5b1b890fca0aa143508a8a6a
Vitor Oliveira <chazan.uni@gmail.com>
2025-08-15 20:07:24 -0300

    config: alterado configurações para incluir o loader de scss para o storybook

--- projects/lib-angular/.storybook/main.ts
+++ projects/lib-angular/.storybook/main.ts
@@ -1,16 +1,49 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-onboarding"
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-docs",
   ],
-  "framework": {
-    "name": "@storybook/angular",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/angular",
+    options: {}
+  },
+  staticDirs: [
+    {
+      from: '../src/lib/assets',
+      to: '/assets',
+    },
+  ],
+
+  webpackFinal: async (config) => {
+    // regra para scss globais (ex.: src/styles.scss)
+    config.module?.rules?.unshift({
+      test: /(?<!\.component)\.scss$/,
+      sideEffects: true,
+      use: [
+        require.resolve('style-loader'),
+        {
+          loader: require.resolve('css-loader'),
+          options: { importLoaders: 2, sourceMap: true },
+        },
+        {
+          loader: require.resolve('resolve-url-loader'),
+          options: { sourceMap: true },
+        },
+        {
+          loader: require.resolve('sass-loader'),
+          options: { sourceMap: true }, // resolve-url-loader precisa de sourceMap true
+        },
+      ],
+    });
+
+    return config;
+  },
 };
+
 export default config;
